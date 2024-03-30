0a097d704d1f8ed0618885777361a4720af94def
koustav29 <koustavkhanda7@gmail.com>
2024-03-31 03:16:23 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if(authUser){
-            const socket = io("http://localhost:8000",{
+            const socket = io("https://chat-app-koustav.onrender.com/",{
                 //sending parameters to socket server for online users
                 query: { userId: authUser._id },
             });
