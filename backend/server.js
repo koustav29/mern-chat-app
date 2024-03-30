b9cad3e170193a1e20749cadc75d21e25f36cc71
koustav29 <koustavkhanda7@gmail.com>
2024-03-31 02:31:15 +0530

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 //package imports
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+
 
 //file imports
 import authRoutes from "../backend/routes/auth.routes.js";
@@ -14,6 +16,8 @@ import { app,server } from "./socket/socket.js";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // it'll allow express to destructure req.body to get data
 //use = middleware to use prefix in routes for more readable codes
 app.use(cookieParser());
@@ -23,10 +27,17 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
-app.post("/", (req, res) => {
-    //root route http://localhost:5000/
-    res.send("Hello world?"+req);
+//express middleware gives us the flexibility to handle static frontend files from backend itself
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend","dist","index.html"));
 });
+
+// app.post("/", (req, res) => {
+//     //root route http://localhost:5000/
+//     res.send("Hello world?"+req);
+// });
 
 server.listen(PORT, () => {
     connectToMongoDB();
